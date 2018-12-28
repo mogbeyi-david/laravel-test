33ce0e6622f5dee368f3577ef4e2f2f8c1046e70
DavidMogbeyi <davidm@l5lab.com>
2018-12-28 15:02:56 +0100

    created the form for creating products

---
+++ resources/views/create-product.blade.php
@@ -0,0 +1,17 @@
+<html>
+<head></head>
+<body>
+<form>
+    <div class="form-group">
+        <input type="text" name="name" class="form-control" placeholder="Name">
+    </div>
+    <div class="form-group">
+        <input type="number" name="quantity_in_stock" class="form-control" placeholder="Quantity in Stock">
+    </div>
+    <div>
+        <input type="number" name="price_per_item" class="form-control" placeholder="Price Per Item">
+    </div>
+    <input type="submit" name="submit" class="btn btn-primary">
+</form>
+</body>
+</html>
