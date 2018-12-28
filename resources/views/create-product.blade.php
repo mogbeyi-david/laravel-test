dca9b650b8ff65ff4e3e3968b01b953793934399
DavidMogbeyi <davidm@l5lab.com>
2018-12-28 16:13:43 +0100

    Submitting the form through ajax

--- resources/views/create-product.blade.php
+++ resources/views/create-product.blade.php
@@ -3,19 +3,19 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
-<form method="post" action="{{ URL::route('create-product') }}">
-    @csrf
+<form method="post" onsubmit="addProduct()">
     <h3>Create New Product</h3>
+    <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
     <div class="form-group">
-        <input type="text" name="name" class="form-control" placeholder="Name">
+        <input type="text" name="name" id="name" class="form-control" placeholder="Name">
     </div>
     <div class="form-group">
-        <input type="number" name="quantity_in_stock" class="form-control" placeholder="Quantity in Stock">
+        <input type="number" name="quantity_in_stock" id="quantity_in_stock" class="form-control" placeholder="Quantity in Stock">
     </div>
     <div>
-        <input type="number" name="price_per_item" class="form-control" placeholder="Price Per Item">
+        <input type="number" name="price_per_item"  id="price_per_item" class="form-control" placeholder="Price Per Item">
     </div>
-    <input type="submit" name="submit" class="btn btn-primary">
+    <button name="submit" class="btn btn-primary" id="js-create-product">Create Product</button>
 </form>
 
 @php $products = \GuzzleHttp\json_decode(Storage::get('products.json')); @endphp
@@ -39,5 +39,32 @@
         </tr>
     @endforeach
 </table>
+<script src="http://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous">
+</script>
+<script>
+  jQuery(document).ready(function(){
+    jQuery('#js-create-product').click(function(e){
+      e.preventDefault();
+      $.ajaxSetup({
+        headers: {
+          'X-CSRF-TOKEN': jQuery('#csrf-token').val()
+        }
+      });
+      jQuery.ajax({
+        url: "{{ url('/create-product') }}",
+        method: 'post',
+        data: {
+          name: jQuery('#name').val(),
+          quantity_in_stock: jQuery('#quantity_in_stock').val(),
+          price_per_item: jQuery('#price_per_item').val()
+        },
+        success: function(result){
+          console.log(result);
+        }});
+    });
+  });
+</script>
 </body>
 </html>
